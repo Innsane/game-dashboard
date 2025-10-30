a081926650c906079fd32e07e1d91d51d9d4e9bc
Innsane <Innsane@users.noreply.github.com>
2025-10-30 22:26:19 +0100

    add enum serialization and databse ensure

--- GameDashboard/Program.cs
+++ GameDashboard/Program.cs
@@ -1,4 +1,7 @@
+using System.Text.Json.Serialization;
 using GameDashboard.Database;
+using GameDashboard.Endpoints;
+using Microsoft.AspNetCore.Http.Json;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,7 +15,13 @@ var connectionString =
     ?? throw new InvalidOperationException("Connection string"
                                            + "'DefaultConnection' not found.");
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlite(connectionString));
+builder.Services.ConfigureHttpJsonOptions(options=> options.SerializerOptions.Converters.Add(new JsonStringEnumConverter()));
+
 var app = builder.Build();
+
+CreateDatabase(app);
+
+app.MapGameEndpoints();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -23,3 +32,10 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.Run();
+
+void CreateDatabase(WebApplication webApplication)
+{
+    using var scope = webApplication.Services.CreateScope();
+    var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    context.Database.EnsureCreated();
+}
